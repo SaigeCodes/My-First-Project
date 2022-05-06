daba83859d26f7353b07f3b0ce0760cd2eb8f327
SaigeCodes <101131901+SaigeCodes@users.noreply.github.com>
2022-05-05 19:12:05 -0500

    Integrating new JavaScript template for forecast
    Also, had to remove and add functioning emojis that pair with the openweatherapi.

--- index.js
+++ index.js
@@ -84,3 +84,35 @@ function getCurrentLocation(event) {
 
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
+
+//Displaying forecast
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temp">
+          <span class="weather-forecast-temp-max"> 75° </span>
+          <span class="weather-forecast-temp-min"> 60° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+displayForecast();
